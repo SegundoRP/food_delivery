c546c420c9db80cede2197c5c4792d0a375eec3f
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-03-22 22:49:51 -0500

    feat: :sparkles: add mehtod for appointments controller

---
+++ appointments_controiller.rb
@@ -0,0 +1,10 @@
+class AppointmentsController
+  def add
+    doctor_id = @view.ask_for_doctor_id
+    patient_id = @view.ask_for_patient_id
+    appointment = Appointment.new
+    appointment.doctor = @doctor_repository.find(doctor_id)
+    appointment.patient = @patient_repository.find(patient_id)
+    @appointment_repository.add(appointment)
+  end
+end
