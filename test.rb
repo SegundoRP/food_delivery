a11671aba492dd2a4317435408a18f8802d0d4d7
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-03-22 22:49:21 -0500

    feat: :sparkles: add csv and modify test file

--- test.rb
+++ test.rb
@@ -1,5 +1,7 @@
 require_relative 'patient'
 require_relative 'room'
+require_relative 'patient_repository'
+require 'csv'
 
 patient = Patient.new(name: "Bob")
 p patient
@@ -8,3 +10,18 @@ room.add_patient(patient)
 # patient.room = room
 p patient.room
 p room
+
+# patients = []
+# CSV.foreach('patients.csv', headers: :first_row, header_converters: :symbol) do |row|
+#   p row
+#   p row[:id] = row[:id].to_i
+#   p row[:name]
+#   p row[:cured] = row[:cured] == 'true'
+#   p row
+#   patients << Patient.new(row)
+#   p patients
+# end
+
+repository = PatientRepository.new('patients.csv')
+repository.create(patient)
+p repository
